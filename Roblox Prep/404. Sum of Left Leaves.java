936752b0dee3bd609d0f1e91aa6aeb3e2328daa9
Bikash Adhikari <bikash30851@gmail.com>
2022-10-02 23:25:12 -0500

    Sum of Left Leaves

---
+++ Roblox Prep/404. Sum of Left Leaves.java
@@ -0,0 +1,54 @@
+/*
+404. Sum of Left Leaves
+
+Given the root of a binary tree, return the sum of all left leaves.
+
+A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+Example 2:
+
+Input: root = [1]
+Output: 0
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        
+        //base case if the node is null;
+        if(root == null) {
+            return 0;
+        }        
+        //if node is a left-leaf node
+        else if (root.left != null && root.left.left == null && root.left.right == null){
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+        
+        //root is the node we are on
+        //if cases above are not true then
+        //if node is not a left leaf node, need to traverse both subtrees and add left leaf nodes recursively.
+        else {
+            return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        }
+    
+    }
+}
